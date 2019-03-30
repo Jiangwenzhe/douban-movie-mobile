ad79fb13a253e8959155c476de90714681e131f2
Erzhuyijian <1821344102@qq.com>
2019-03-30 22:22:30 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -98,12 +98,97 @@ var topMovie = {
 var onTheater = {
   init: function(){
     console.log('onTheater')
+    this.$ontheater = $('#ontheater')
+    this.index = 0
+    this.isLoading = false
+    this.isFinish = false
+    this.bind()
+    this.loadAndrender()
   },
   bind: function(){
+    var _this = this
+    window.addEventListener('scroll',function(){
+      if(_this.isOnBottom()){
+         _this.loadAndrender()
+      }
+    })
+  },
+  loadAndrender: function(){
+    var _this = this
+    this.loadData(function(data){
+      _this.renderData(data)
+    })
+  },
+  loadData: function(callback) {
+    console.log('loading data...')
+    var _this = this
+    if(_this.isLoading) {
+      return;
+    }
+    _this.isLoading = true
+    _this.$ontheater.parents().find('.loading').show()
+    $.ajax({
+      url: 'https://api.douban.com/v2/movie/in_theaters',
+      type: 'GET',
+      data: {
+        start: _this.index || 0,
+        count: 20
+      },
+      dataType: 'jsonp'
+    }).done(function (ret) {
+      console.log(ret)
+      _this.index+=20
+      if(_this.index >= ret.total) {
+        _this.isFinish = true
+      }
+      callback&&callback(ret)
+    }).fail(function () {
+      console.log('top get data error')
+    }).always(function(){
+      _this.isLoading = false
+      _this.$ontheater.parents().find('.loading').hide()
+    })
 
   },
-  start: function(){
-
+  renderData: function(data) {
+    console.log('rendering data...')
+    var _this = this
+    data.subjects.forEach(function (movie) {
+      var cardhtml = '<div class="card"> \
+      <a href="https://movie.douban.com/subject/1292052/"> \
+        <div class="cover"> \
+          <img  src=""  alt=""> \
+        </div> \
+        <div class="detail"> \
+          <h2></h2>\
+          <div class="extra"><span class="score"></span> / <span class="collection"></span> 收藏</div>\
+          <div class="extra"><span class="year"></span> / <span class="type"></span></div>\
+          <div class="extra"><span class="director"></span> </div>\
+          <div class="extra"><span class="casts"></span></div>\
+          <div class="extra"></div> \
+        </div> \
+      </a> \
+    </div>'
+      var card = $(cardhtml)
+      card.find('a').attr('href', movie.alt)
+      card.find('.cover img').attr('src',movie.images.medium)
+      card.find('h2').text(movie.title)
+      card.find('.score').text(movie.rating.average)
+      card.find('.collection').text(movie.collect_count)
+      card.find('.year').text(movie.year)
+      card.find('.type').text(movie.genres.join('、'))
+      card.find('.director').text('导演：'+movie.directors.map(function(director){
+        return director.name
+      }).join('、'))
+      card.find('.casts').text('演员：'+movie.casts.map(function(cast){
+        return cast.name
+      }).join('、'))
+      _this.$ontheater.append(card)
+    })
+  }
+  ,
+  isOnBottom: function() {
+    return ($(document).height()-20 <= $(window).scrollTop() + $(window).height())
   }
 }
 
